10edbe0020fa89e9d398ff5c2ed094d7a6b7c80d
Pedro Vitor Santos <102162851+PVeeeeeee@users.noreply.github.com>
2022-09-07 11:02:15 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,18 +15,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
        // \App\Models\User::factory(10)->create();
+        
+        \App\Models\UserType::factory()->create([
+            'type'=>'Aluno'
+       ]);
+       \App\Models\UserType::factory()->create([
+        'type'=>'Professor'
+        ]);
 
         \App\Models\User::factory()->create([
             'name' => 'Teacher',
             'email' => 'prof@gmail.com',
             'password' => \Illuminate\Support\Facades\Hash::make('adminpass'),
             'ust_id'=>2,
-        ]);
-        \App\Models\UserType::factory()->create([
-            'type'=>'Aluno'
-       ]);
-       \App\Models\UserType::factory()->create([
-        'type'=>'Professor'
         ]);
     }
 }
